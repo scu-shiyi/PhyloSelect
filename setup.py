406343d2757cd626935b47fef44ea79e0bef6dd7
Evanstone <shi@stu.scu.edu.cn>
2026-05-26 16:50:23 +0800

    Update package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="phyloselect",
-    version="1.1.0",
+    version="1.1.1",
     description="PhyloSelect: A comprehensive toolkit for phylogenetic analysis and evolutionary selection.",
     author="scu-shiyi",
     author_email="shi@stu.scu.edu.cn",
